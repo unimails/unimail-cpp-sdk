8f484a58211be5a3e68dfcb6ab73b70f25106f62
i-curve <2517094646@qq.com>
2025-04-21 14:19:28 +0800

    fix: fix vcpkg import error
    Signed-off-by: i-curve <2517094646@qq.com>

--- cmake/unimail-cpp-sdkConfig.cmake
+++ cmake/unimail-cpp-sdkConfig.cmake
@@ -1,4 +1,7 @@
 @PACKAGE_INIT@
 
-include("${CMAKE_CURRENT_LIST_DIR}/unimailCppSdkTargets.cmake")
+find_package(nlohmann_json CONFIG REQUIRED)
+find_package(restclient-cpp CONFIG REQUIRED)
+
+include("${CMAKE_CURRENT_LIST_DIR}/unimail-cpp-sdkTargets.cmake")
 check_required_components("@PROJECT_NAME@")
